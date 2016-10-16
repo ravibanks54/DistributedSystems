ebc126a39b6d4c00d1d5f9d108244d547c6c19a8
Ravi Bhankharia <ravibanks54@gmail.com>
2016-10-16 17:20:18 -0400

    Deleted Sample stuff, and added comments to client

--- Assignment1/rmi/Client.java
+++ Assignment1/rmi/Client.java
@@ -1,40 +1,39 @@
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.rmi.Naming;
-import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import static java.rmi.registry.LocateRegistry.getRegistry;
 
 /**
  * Created by ravibhankharia on 10/5/16.
  */
 public class Client {
-    static Registry registry;
+    static Registry registry;       //Registry variable to hold the RMI Registry. Declared static to prevent
+                                    //garbage collection
     public static void main(String args[]) {
         try {
-            if (args.length < 2) {
+            if (args.length < 2) {      //Not enough arguments
                 System.err.println("usage: java SampleClient -h rmiregistryserver -p registry_port city state \n");
                 System.exit(1);
             }
-            String host = "localhost";
-            int port = 1099;
+            String host = "localhost";  //Set default host as localhost
+            int port = 1099;            //Set default port as 1099
             String city = "";
             String state = "";
 
-            for (int i = 0; i < args.length; i++) {     //Whichever order the flags are in, it'll parse them
-                if (args[i].equalsIgnoreCase("-h")) {
+            for (int i = 0; i < args.length; i++) {     //Parse all flags
+                if (args[i].equalsIgnoreCase("-h")) {   //Check if host is specified. If so overwrite host string
+                    if (args.length <= i + 1) {         //Check if the flag is specified without an argument
+                        System.err.println("usage: java SampleClient -h rmiregistryserver -p registry_port city state \n");
+                        System.exit(1);
+                    }
+                    host = args[i + 1];     //Set the hostname from the arguments
+                } else if (args[i].equalsIgnoreCase("-p")) {    //Check if port is specified. If so overwrite port int
                     if (args.length <= i + 1) {
                         System.err.println("usage: java SampleClient -h rmiregistryserver -p registry_port city state \n");
                         System.exit(1);
                     }
-                    host = args[i + 1];
-                } else if (args[i].equalsIgnoreCase("-p")) {
-                    if (args.length <= i + 1) {
-                        System.err.println("usage: java SampleClient -h rmiregistryserver -p registry_port city state \n");
-                        System.exit(1);
-                    }
-                    port = Integer.parseInt(args[i + 1]);
+                    port = Integer.parseInt(args[i + 1]);   //Set the port from the arguments
                 }
             }
+            //Set the city and state from the arguments depending on the number of flags specified
             if (args.length == 2){
                 city = args[0];
                 state = args[1];
@@ -45,14 +44,15 @@ public class Client {
                 city = args[4];
                 state = args[5];
             }
-            registry = LocateRegistry.getRegistry(port);
-            String urlPlaces = "//" + host + ":" + port + "/Places";
+            registry = getRegistry(port);       //Create the RMI Registry with the given port
+            String urlPlaces = "//" + host + ":" + port + "/Places";    //Set the lookup url
             System.out.println("looking up " + urlPlaces);
             //PlaceInterface place = (PlaceInterface) Naming.lookup(urlPlaces);
-            PlaceInterface place = (PlaceInterface) registry.lookup(urlPlaces);
+            PlaceInterface place = (PlaceInterface) registry.lookup(urlPlaces);     //Initialize the interface
+
 
                 // call the remote method and print the return
-            PlaceStruct placeStruct = place.findPlace(city, state);
+            PlaceStruct placeStruct = place.findPlace(city, state);     //Search for the specified city and state
             if (placeStruct == null){
                 System.out.println("Place not found - Either place does not exist or error retrieving Places file");
                 return;
@@ -61,11 +61,11 @@ public class Client {
             }
 
             //port--;
-            String urlAirports = "//" + host + ":" + port + "/Airports";
+            String urlAirports = "//" + host + ":" + port + "/Airports";        //Form the lookup url
             System.out.println("looking up " + urlAirports);
             //AirportInterface airport = (AirportInterface) Naming.lookup(urlAirports);
-            AirportInterface airport = (AirportInterface) registry.lookup(urlAirports);
-            // call the remote method and print the return
+            AirportInterface airport = (AirportInterface) registry.lookup(urlAirports);     //Initialize the Airports interface
+            // Call the remote method and print the return
             AirportStruct[] airportStructs = airport.getAirports(placeStruct.lat, placeStruct.lon);
             if(airportStructs == null){
                 System.out.println("Error retrieving Airport data");
@@ -73,16 +73,11 @@ public class Client {
             }
             for (int i = 0; i < airportStructs.length; i++){
 
-                System.out.println(airportStructs[i].toString());
+                System.out.println(airportStructs[i].toString());       //Output the five closes airports
             }
 
         } catch(Exception e) {
             System.out.println("Client exception: " + e);
-/*            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            System.out.println(sw.toString()); // stack trace as a string
-            //System.out.println(e.getStackTrace());*/
 
         }
     }
--- Assignment1/rmi/SampleInterface.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface SampleInterface extends Remote {
-	public String invert(String msg) throws RemoteException;
-}
